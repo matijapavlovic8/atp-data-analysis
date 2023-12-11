7bacd73e95f588419d2d65f5d054c408533f25be
MatijaP <matija.p71@gmail.com>
2023-12-11 17:05:56 +0100

    Initial

--- main.R
+++ main.R
@@ -1,1 +1,22 @@
-proba <- read.csv("dataset/atp_matches_1968.csv")
+all_matches <- data.frame()
+
+for (year in 1968:2023) {
+  file_name <- paste0("dataset/atp_matches_", year, ".csv")
+
+  matches_year <- read.csv(file_name, stringsAsFactors = FALSE)
+
+  all_matches <- rbind(all_matches, matches_year)
+}
+
+print(str(all_matches))
+
+print(names(all_matches))
+
+print(summary(all_matches))
+
+for (col in names(all_matches)) {
+  print(paste("Summary for column", col))
+  print(summary(all_matches[[col]]))
+}
+
+
